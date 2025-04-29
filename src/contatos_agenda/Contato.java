2e09318203b24423406bdba3d9d51e4a72f8f204
Luiz Felipe Santos Lopes <luizphelip1998@gmail.com>
2025-04-29 20:53:36 -0300

    Criando uma classe para os contatos (Contato.java)

--- src/contatos_agenda/Contato.java
+++ src/contatos_agenda/Contato.java
@@ -1,4 +1,24 @@
 package contatos_agenda;
 
 public class Contato {
+    private String nome;
+    private Integer telefone;
+
+    public Contato(String nome, Integer telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getTelefone() {
+        return telefone;
+    }
+
+    @Override
+    public String toString() {
+        return "Contato: " + nome + " --- Telefone: " + telefone;
+    }
 }
